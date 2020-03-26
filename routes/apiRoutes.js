f40cd3aaf824011d0f40e17710855715f3b0db6e
Shawn Wright <volushawn@gmail.com>
2020-03-25 21:55:32 -0500

    Most recent workout served to index page from API route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,18 @@ const path = require("path");
 
 module.exports = function(app) {
 
+    
+    //get all workouts
     app.get("/api/workouts/range", function(req, res) {
+        db.Workout.find({}, function (err, docs) {
+            if (err) throw err;
+            res.json(docs);
+        })
+    });
+    
+    
+    //get most recent workout
+    app.get("/api/workouts/", function(req, res) {
         db.Workout.find({}, function (err, docs) {
             if (err) throw err;
             res.json(docs);
